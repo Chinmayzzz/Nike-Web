05865742ea00188ce74943c77d87a2fb0b2c6d6e
Chinmay Bokolia <bokoliachinmay@gmail.com>
2024-07-29 23:55:42 +0530

    Popular Product section made

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,23 +32,23 @@ export const statistics = [
 export const products = [
     {
         imgURL: shoe4,
-        name: "Nike Air Jordan-01",
-        price: "$200.20",
+        name: "Nike Zoom Pegasus",
+        price: "Rs.4999/-",
     },
     {
         imgURL: shoe5,
-        name: "Nike Air Jordan-10",
-        price: "$210.20",
+        name: "Nike Dunks Low",
+        price: "Rs.5999/-",
     },
     {
         imgURL: shoe6,
-        name: "Nike Air Jordan-100",
-        price: "$220.20",
+        name: "Nike Air Jordan",
+        price: "Rs.3599/-",
     },
     {
         imgURL: shoe7,
-        name: "Nike Air Jordan-001",
-        price: "$230.20",
+        name: "Nike UNC's",
+        price: "Rs.7999/-",
     },
 ];
 
